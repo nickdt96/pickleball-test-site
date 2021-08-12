de7120248d5eabf1e60a0a1a14615f5f49a72ea6
John Adamchak <johnray987@yahoo.com>
2021-08-11 23:18:31 -0500

    configuration of mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/common/Header";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 
 function App() {
+
+  const theme = createMuiTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <Typography variant="h2" color="primary">
+      Hello World
+    </Typography>
+  )
+}
+
+export default Header
